26ff37e8496ee8a1bb3a9fde8c617ba694353872
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2025-08-31 13:53:53 +0530

    Update Clients.js

--- src/Component/Clients.js
+++ src/Component/Clients.js
@@ -13,40 +13,43 @@ import client7 from "../Component/Client/7.jpg";
 
 function Clients() {
   const clients = [
-    { name: "Wonderlamp Trading Academy", image: client7 },
-    { name: "Makkal Nethi Maiyam", image: client3 },
-    { name: "MS Events", image: client6 },
-    { name: "Isha Hair Oil", image: client4 },
-    { name: "TM Machine", image: client2 },
-    { name: "Eagle Eye", image: client5 },
-    { name: "Aram Alternative Foods", image: client1 },
+    { id: 1, name: "Wonderlamp Trading Academy", image: client7 },
+    { id: 2, name: "Makkal Nethi Maiyam", image: client3 },
+    { id: 3, name: "MS Events", image: client6 },
+    { id: 4, name: "Isha Hair Oil", image: client4 },
+    { id: 5, name: "TM Machine", image: client2 },
+    { id: 6, name: "Eagle Eye", image: client5 },
+    { id: 7, name: "Aram Alternative Foods", image: client1 },
   ];
 
   const settings = {
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 4, // lg
+    slidesToScroll: 2,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1024, // md
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768, // sm
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 576, // xs
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -58,35 +61,29 @@ function Clients() {
   return (
     <div className="container my-5">
       <h2 className="text-center mb-4">Our Clients</h2>
-      <p className="text-center mb-2">Trusted by businesses & organizations</p>
+      <p className="text-center mb-3">Trusted by businesses & organizations</p>
 
-      <Slider {...settings}>
-        {clients.map((client, index) => (
-          <div
-            key={index}
-            className="d-flex justify-content-center align-items-center p-2"
-          >
-            <div
-              className="card shadow text-center h-100"
-              style={{ height: "320px" }}
-            >
-              <img
-                src={client.image}
-                alt={client.name}
-                className="card-img-top"
-                style={{
-                  height: "120px",
-                  objectFit: "contain",
-                  padding: "15px",
-                }}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{client.name}</h5>
+      <div className="row">
+        <div className="col">
+          <Slider {...settings}>
+            {clients.map((client) => (
+              <div key={client.id} className="px-2"> {/* spacing */}
+                <div className="card text-center shadow h-100">
+                  <div className="p-3">
+                    <img
+                      src={client.image}
+                      alt={client.name}
+                      className="mx-auto d-block"
+                      style={{ height: "100px", width: "120px", objectFit: "contain" }}
+                    />
+                    <h6 className="mt-3">{client.name}</h6>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </Slider>
+            ))}
+          </Slider>
+        </div>
+      </div>
     </div>
   );
 }
